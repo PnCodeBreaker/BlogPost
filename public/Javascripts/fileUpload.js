462ea1f888fb9aa51dc1cceb0e4c2c081c7c1ea3
PnCodeBreaker <prithwishnp.n@gmail.com>
2021-02-14 22:50:09 +0530

    reducing image size

--- public/Javascripts/fileUpload.js
+++ public/Javascripts/fileUpload.js
@@ -2,5 +2,11 @@ FilePond.registerPlugin(
     FilePondPluginImagePreview,
     FilePondPluginImageResize,
     FilePondPluginFileEncode,
+    FilePondPluginImageTransform
 )
+FilePond.setOptions({
+    imageResizeTargetHeight: 720,
+    imageResizeUpscale: false,
+    allowImageTransform: true,
+})
 FilePond.parse(document.body);
